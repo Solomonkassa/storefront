0da38a6f5da53ceba7c3bfc2177f9a7943bc5844
solomonkassa <solomonmulu000@gmail.com>
2023-10-24 13:09:28 +0300

    resolving-circular-relationships

--- storefront/store/models.py
+++ storefront/store/models.py
@@ -1,12 +1,25 @@
 from django.db import models
 
+class Promotion(models.Model):
+    description = models.CharField(max_length=255)
+    discount = models.FloatField()
+    
+    
+class Collection(models.Model):
+    title = models.CharField(max_length=255)
+    # Resolving Circular relationships
+    featured_product = models.ForeignKey('Product', on_delete=models.SET_NULL,null=True, related_name='+')
+
+
 class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
-
+    Collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
+    promotions = models.ManyToManyField(Promotion)
+    
 class Customer(models.Model):
     MEMBERSHIP_BRONZE = 'B'
     MEMBERSHIP_SILVER ='S'
@@ -32,15 +45,31 @@ class Order(models.Model):
     
     PAYMENT_STATUS_CHOICE = [
         (PAYMENT_STATUS_PENDING, 'Pending'),
-        (PAYMENT_STATUS_COMPLETE, 'Complete')
+        (PAYMENT_STATUS_COMPLETE, 'Complete'),
         (PAYMENT_STATUS_FAILED, 'Failed')
     ]
     
     placed_at = models.DateTimeField(auto_now_add=True)
     payment_status = models.CharField(max_length=1, choices=PAYMENT_STATUS_CHOICE, default=PAYMENT_STATUS_PENDING )
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
     
-    #One to one Relationship
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.PROTECT)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    quantity = models.PositiveSmallIntegerField()
+    unit_price = models.DecimalField(max_digits=6,decimal_places=2)
+    
+    
+    
 class Address(models.Model):
     street = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
-    customer = models.OneToOneField(Customer, on_delete=models.CASCADE, primary_key=True)    
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)  
+    
+class Cart(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True) 
+    
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE) 
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField() 
