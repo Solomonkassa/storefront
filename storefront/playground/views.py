e48df77647af6dee134f9df6c20023a0d48ddecd
solomonkassa <solomonmulu000@gmail.com>
2023-10-25 18:59:08 +0300

    connecting-database

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.db.models.aggregates import Count, Sum, Avg, Max, Min
+from django.db.models.functions import Concat
+from django.db.models import Value, F, Func
+from store.models import Product, Customer
+
+
 
 
 def SayHello(request):
-    return render(request,'hello.html', {'name': 'Solomon'})
+    
+    query_set = Customer.objects.annotate(
+        fullname = Concat(('first_name'),Value(' '),('last_name'))
+    )
+        
+    return render(request,'hello.html', {'Customers': list(query_set) })
 
 
