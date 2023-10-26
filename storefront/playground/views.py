59f5a63e51831c25b714d29031a27519ea6d5654
solomonkassa <solomonmulu000@gmail.com>
2023-10-26 06:39:19 +0300

    tagged_item

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.db.models import Value, F, ExpressionWrapper
+from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from store.models import Product, Customer
+from tags.models import TaggedItem
 
 def SayHello(request):
-    return render(request,'hello.html', {'name': 'Solomon'})
+   
+   content_type = ContentType.objects.get_for_model(Product)
+   queryset =TaggedItem.objects.select_related('tag').filter(
+       content_type=content_type,
+       object_id=1
+   )
+   return render(request,'hello.html', {'Product': list(queryset)})
 
 
+    
