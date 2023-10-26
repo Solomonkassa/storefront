e435b3ad019d8bed06ebd2626df1f70ceb156031
solomonkassa <solomonmulu000@gmail.com>
2023-10-26 07:04:59 +0300

    merge

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-<<<<<<< HEAD
 from django.db.models.aggregates import Count, Sum, Avg, Max, Min
 from django.db.models.functions import Concat
 from django.db.models import Value, F, Func
@@ -16,22 +15,6 @@ def SayHello(request):
     )
         
     return render(request,'hello.html', {'Customers': list(query_set) })
-=======
-from django.db.models import Value, F, ExpressionWrapper
-from django.db import models
-from django.contrib.contenttypes.models import ContentType
-from store.models import Product, Customer
-from tags.models import TaggedItem
-
-def SayHello(request):
-   
-   content_type = ContentType.objects.get_for_model(Product)
-   queryset =TaggedItem.objects.select_related('tag').filter(
-       content_type=content_type,
-       object_id=1
-   )
-   return render(request,'hello.html', {'Product': list(queryset)})
->>>>>>> backup-main
 
 
     
